e588bc97c314e52af9a2f4b123e0f3340d963d08
moratori <hoko1993@yahoo.co.jp>
2025-09-13 13:56:34 +0900

    android app initial commmit

---
+++ android/v1/app/src/main/java/com/example/moproapp/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.moproapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import java.io.File
+import java.io.IOException
+import java.io.InputStream
+import java.io.OutputStream
+
+@Throws(IOException::class)
+fun copyFile(inputStream: InputStream, outputStream: OutputStream) {
+    val buffer = ByteArray(1024)
+    var read: Int
+    while (inputStream.read(buffer).also { read = it } != -1) {
+        outputStream.write(buffer, 0, read)
+    }
+}
+
+@Composable
+fun getFilePathFromAssets(name: String): String {
+    val context = LocalContext.current
+    return remember {
+        val assetManager = context.assets
+        val inputStream = assetManager.open(name)
+        val file = File(context.filesDir, name)
+        copyFile(inputStream, file.outputStream())
+        file.absolutePath
+    }
+}
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MainScreen()
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .padding(innerPadding)
+        ) {
+            NoirComponent()
+        }
+    }
+}
