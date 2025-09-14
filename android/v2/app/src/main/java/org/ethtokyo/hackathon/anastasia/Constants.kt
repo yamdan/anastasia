22e9e7775157e298e96f2f098ce51785fe5d8daf
moratori <hoko1993@yahoo.co.jp>
2025-09-14 20:54:31 +0900

    Add smart-contract address

--- android/v2/app/src/main/java/org/ethtokyo/hackathon/anastasia/Constants.kt
+++ android/v2/app/src/main/java/org/ethtokyo/hackathon/anastasia/Constants.kt
@@ -3,4 +3,6 @@ package org.ethtokyo.hackathon.anastasia
 object Constants {
     const val KEY_ALIAS = "secure-key";
     const val INFURA_ENDPOINT_URL = "https://sepolia.infura.io/v3/"
+    const val SMART_CONTRACT_ADDRESS_CA = "0x6d8f199B5fA030DCD4b6Afce600B35AdeB78d155"
+    const val SMART_CONTRACT_ADDRESS_EE = ""
 }
