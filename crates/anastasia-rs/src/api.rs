2ae2ccff1fd00f8ef1f0c9515d2ce6556a2bb8b2
Dan Yamamoto <dan@iij.ad.jp>
2025-09-24 18:04:48 +0900

    refactor(anastasia-rs): Update `commit_attrs` function parameters for clarity and consistency and revise tests

--- crates/anastasia-rs/src/api.rs
+++ crates/anastasia-rs/src/api.rs
@@ -14,11 +14,11 @@ pub struct CommitResult {
 }
 
 pub fn commit_attrs(
-    subject: Vec<u8>,
-    subject_key_identifier: Vec<u8>,
-    subject_pk_x: Vec<u8>,
-    subject_pk_y: Vec<u8>,
-    r: Option<String>,
+    distinguished_name: &Vec<u8>,
+    key_id: &Vec<u8>,
+    pk_x: &Vec<u8>,
+    pk_y: &Vec<u8>,
+    r: Option<&str>,
 ) -> Result<CommitResult, String> {
     let mut rng = OsRng;
     let r = if let Some(r_hex) = r {
@@ -30,24 +30,25 @@ pub fn commit_attrs(
 
     let cmt = utils::commit_attrs(
         {
-            let mut padded_subject = subject.clone();
-            if padded_subject.len() > 124 {
-                return Err("subject must be at most 124 bytes".to_string());
+            let mut padded_dn = distinguished_name.clone();
+            if padded_dn.len() > 124 {
+                return Err("distinguished name must be at most 124 bytes".to_string());
             }
-            padded_subject.resize(124, 0u8);
-            padded_subject
+            padded_dn.resize(124, 0u8);
+            padded_dn
                 .try_into()
-                .map_err(|_| "subject must be at most 124 bytes".to_string())?
+                .map_err(|_| "distinguished name must be at most 124 bytes".to_string())?
         },
-        subject_key_identifier
+        key_id
+            .as_slice()
             .try_into()
-            .map_err(|_| "subject_key_identifier must be 20 bytes".to_string())?,
-        subject_pk_x
+            .map_err(|_| "key_identifier must be 20 bytes".to_string())?,
+        pk_x.as_slice()
             .try_into()
-            .map_err(|_| "subject_pk_x must be 32 bytes".to_string())?,
-        subject_pk_y
+            .map_err(|_| "publickey_x must be 32 bytes".to_string())?,
+        pk_y.as_slice()
             .try_into()
-            .map_err(|_| "subject_pk_y must be 32 bytes".to_string())?,
+            .map_err(|_| "publickey_y must be 32 bytes".to_string())?,
         r,
     )?;
 
@@ -109,7 +110,7 @@ mod tests {
             0x6f, 0x6f, 0x67, 0x6c, 0x65, 0x20, 0x4c, 0x4c, 0x43, 0x31, 0x12, 0x30, 0x10, 0x06,
             0x03, 0x55, 0x04, 0x03, 0x13, 0x09, 0x44, 0x72, 0x6f, 0x69, 0x64, 0x20, 0x43, 0x41,
             0x33,
-        ];
+        ]; // O=Google LLC, CN=Droid CA3
         let subject_key_identifier = vec![
             0xfe, 0x62, 0x6c, 0xdc, 0x2a, 0xe5, 0x80, 0xe7, 0x19, 0x6a, 0xca, 0x23, 0xdd, 0x23,
             0xf1, 0x39, 0x02, 0x46, 0xa8, 0xa5,
@@ -126,15 +127,13 @@ mod tests {
         ];
 
         let CommitResult { cmt, r } = commit_attrs(
-            subject,
-            subject_key_identifier,
-            subject_pk_x,
-            subject_pk_y,
+            &subject,
+            &subject_key_identifier,
+            &subject_pk_x,
+            &subject_pk_y,
             None,
         )
         .unwrap();
-        println!("Commitment: {}", cmt);
-        println!("Randomness: {}", r);
         assert_eq!(cmt.len(), 64); // 32 bytes in hex
         assert_eq!(r.len(), 64); // 32 bytes in hex
     }
@@ -146,7 +145,7 @@ mod tests {
             0x6f, 0x6f, 0x67, 0x6c, 0x65, 0x20, 0x4c, 0x4c, 0x43, 0x31, 0x12, 0x30, 0x10, 0x06,
             0x03, 0x55, 0x04, 0x03, 0x13, 0x09, 0x44, 0x72, 0x6f, 0x69, 0x64, 0x20, 0x43, 0x41,
             0x33,
-        ];
+        ]; // O=Google LLC, CN=Droid CA3
         let subject_key_identifier = vec![
             0xfe, 0x62, 0x6c, 0xdc, 0x2a, 0xe5, 0x80, 0xe7, 0x19, 0x6a, 0xca, 0x23, 0xdd, 0x23,
             0xf1, 0x39, 0x02, 0x46, 0xa8, 0xa5,
@@ -161,13 +160,13 @@ mod tests {
             0xbb, 0x70, 0xab, 0xf5, 0xb1, 0xb3, 0x5a, 0xc1, 0xe3, 0xb8, 0x95, 0xee, 0x2e, 0xc0,
             0xa8, 0x5a, 0xfb, 0xd2,
         ];
-        let r = "deadbeef".to_string();
+        let r = "deadbeef";
 
         let CommitResult { cmt, r } = commit_attrs(
-            subject,
-            subject_key_identifier,
-            subject_pk_x,
-            subject_pk_y,
+            &subject,
+            &subject_key_identifier,
+            &subject_pk_x,
+            &subject_pk_y,
             Some(r),
         )
         .unwrap();
@@ -189,14 +188,16 @@ mod tests {
         )
         .unwrap();
         let cert = std::fs::read("test_data/es256_ca.der").unwrap();
-        println!(
-            "cert = [{}];",
-            cert.iter()
-                .map(|b| format!("0x{:02x}", b))
-                .collect::<Vec<_>>()
-                .join(", ")
-        );
+
         let now = Some(Utc.with_ymd_and_hms(2025, 9, 15, 0, 0, 0).unwrap());
+
+        // Generate previous commitment
+        let issuer = vec![
+            0x30, 0x29, 0x31, 0x13, 0x30, 0x11, 0x06, 0x03, 0x55, 0x04, 0x0a, 0x13, 0x0a, 0x47,
+            0x6f, 0x6f, 0x67, 0x6c, 0x65, 0x20, 0x4c, 0x4c, 0x43, 0x31, 0x12, 0x30, 0x10, 0x06,
+            0x03, 0x55, 0x04, 0x03, 0x13, 0x09, 0x44, 0x72, 0x6f, 0x69, 0x64, 0x20, 0x43, 0x41,
+            0x33,
+        ]; // O=Google LLC, CN=Droid CA3        
         let authority_key_id = vec![
             0xfe, 0x62, 0x6c, 0xdc, 0x2a, 0xe5, 0x80, 0xe7, 0x19, 0x6a, 0xca, 0x23, 0xdd, 0x23,
             0xf1, 0x39, 0x02, 0x46, 0xa8, 0xa5,
@@ -211,8 +212,20 @@ mod tests {
             0xbb, 0x70, 0xab, 0xf5, 0xb1, 0xb3, 0x5a, 0xc1, 0xe3, 0xb8, 0x95, 0xee, 0x2e, 0xc0,
             0xa8, 0x5a, 0xfb, 0xd2,
         ];
-        let prev_cmt = "0ede28f511104f08069e07986707873be5cbba917f02f02407ad1fdd6838679b";
         let prev_cmt_r = "deadbeef";
+        let CommitResult {
+            cmt: prev_cmt,
+            r: _,
+        } = commit_attrs(
+            &issuer,
+            &authority_key_id,
+            &issuer_pk_x,
+            &issuer_pk_y,
+            Some(prev_cmt_r),
+        )
+        .unwrap();
+
+        // Generate proof
         let ProofResult {
             proof,
             next_cmt,
@@ -229,13 +242,48 @@ mod tests {
         )
         .unwrap();
 
-        println!("Next commitment: {}", next_cmt);
-        println!("Next commitment randomness: {}", next_cmt_r);
-        println!("Proof (hex): {}", hex::encode(&proof));
-        println!("Proof length: {}", proof.len());
-        assert!(!proof.is_empty());
+        assert!(!proof_with_public_inputs.proof.is_empty());
         assert_eq!(next_cmt.len(), 64); // 32 bytes in hex
-        assert_eq!(next_cmt_r.len(), 64); // 32 bytes in hex        
+        assert_eq!(next_cmt_r.len(), 64); // 32 bytes in hex
+        assert_eq!(
+            proof_with_public_inputs.num_public_inputs,
+            9 // Number of public inputs expected for es256_ca
+        );
+
+        // Generate next commitment and check it matches
+        let subject = vec![
+            0x30, 0x3f, 0x31, 0x29, 0x30, 0x27, 0x06, 0x03, 0x55, 0x04, 0x03, 0x13, 0x20, 0x65,
+            0x35, 0x62, 0x66, 0x61, 0x39, 0x37, 0x37, 0x31, 0x35, 0x63, 0x31, 0x63, 0x62, 0x31,
+            0x31, 0x37, 0x30, 0x63, 0x33, 0x30, 0x65, 0x30, 0x31, 0x33, 0x33, 0x31, 0x65, 0x65,
+            0x66, 0x34, 0x32, 0x31, 0x12, 0x30, 0x10, 0x06, 0x03, 0x55, 0x04, 0x0a, 0x13, 0x09,
+            0x53, 0x74, 0x72, 0x6f, 0x6e, 0x67, 0x42, 0x6f, 0x78,
+        ]; // CN=e5bfa97715c1cb1170c30e01331eef42, O=StrongBox
+        let subject_key_id = vec![
+            0x83, 0x29, 0xbe, 0xbb, 0x68, 0xbc, 0x24, 0xed, 0x89, 0x38, 0x4d, 0xb4, 0xf1, 0x94,
+            0x6c, 0x20, 0xd7, 0x95, 0x9a, 0x05,
+        ];
+        let subject_pk_x = vec![
+            0xa3, 0x30, 0xd2, 0x88, 0x45, 0xc2, 0xf4, 0xb1, 0x60, 0xa7, 0xa5, 0xa8, 0xec, 0x1e,
+            0x46, 0x21, 0x31, 0x18, 0x5e, 0x25, 0xba, 0x48, 0x7e, 0xba, 0x2f, 0xbb, 0x41, 0xd7,
+            0x18, 0xa7, 0xa6, 0xbf,
+        ];
+        let subject_pk_y = vec![
+            0xd7, 0x87, 0x8d, 0xc6, 0x36, 0xe4, 0x1e, 0xa4, 0xe2, 0x51, 0x6a, 0xa9, 0xc4, 0xf7,
+            0x1f, 0xce, 0x15, 0xf5, 0xd2, 0x48, 0x34, 0x05, 0x82, 0x56, 0x99, 0x72, 0x5c, 0xb1,
+            0x3c, 0xeb, 0x47, 0xcd,
+        ];
+        let CommitResult {
+            cmt: next_cmt_generated,
+            r: _,
+        } = commit_attrs(
+            &subject,
+            &subject_key_id,
+            &subject_pk_x,
+            &subject_pk_y,
+            Some(&next_cmt_r),
+        )
+        .unwrap();
+        assert_eq!(next_cmt, next_cmt_generated);
     }
 
     #[test]
@@ -248,14 +296,17 @@ mod tests {
         )
         .unwrap();
         let cert = std::fs::read("test_data/es256_ee.der").unwrap();
-        println!(
-            "cert = [{}];",
-            cert.iter()
-                .map(|b| format!("0x{:02x}", b))
-                .collect::<Vec<_>>()
-                .join(", ")
-        );
+
         let now = Some(Utc.with_ymd_and_hms(2025, 9, 15, 0, 0, 0).unwrap());
+
+        // Generate previous commitment
+        let issuer = vec![
+            0x30, 0x3f, 0x31, 0x29, 0x30, 0x27, 0x06, 0x03, 0x55, 0x04, 0x03, 0x13, 0x20, 0x65,
+            0x35, 0x62, 0x66, 0x61, 0x39, 0x37, 0x37, 0x31, 0x35, 0x63, 0x31, 0x63, 0x62, 0x31,
+            0x31, 0x37, 0x30, 0x63, 0x33, 0x30, 0x65, 0x30, 0x31, 0x33, 0x33, 0x31, 0x65, 0x65,
+            0x66, 0x34, 0x32, 0x31, 0x12, 0x30, 0x10, 0x06, 0x03, 0x55, 0x04, 0x0a, 0x13, 0x09,
+            0x53, 0x74, 0x72, 0x6f, 0x6e, 0x67, 0x42, 0x6f, 0x78,
+        ]; // CN=e5bfa97715c1cb1170c30e01331eef42, O=StrongBox
         let authority_key_id = vec![
             0x83, 0x29, 0xbe, 0xbb, 0x68, 0xbc, 0x24, 0xed, 0x89, 0x38, 0x4d, 0xb4, 0xf1, 0x94,
             0x6c, 0x20, 0xd7, 0x95, 0x9a, 0x05,
@@ -270,8 +321,20 @@ mod tests {
             0x1f, 0xce, 0x15, 0xf5, 0xd2, 0x48, 0x34, 0x05, 0x82, 0x56, 0x99, 0x72, 0x5c, 0xb1,
             0x3c, 0xeb, 0x47, 0xcd,
         ];
-        let prev_cmt = "2a296b0c9a2c8b4c6c56357c632860849f42a4defa6b491b2421a962a3543f5c";
         let prev_cmt_r = "feedface";
+        let CommitResult {
+            cmt: prev_cmt,
+            r: _,
+        } = commit_attrs(
+            &issuer,
+            &authority_key_id,
+            &issuer_pk_x,
+            &issuer_pk_y,
+            Some(prev_cmt_r),
+        )
+        .unwrap();
+
+        // Generate proof
         let ProofResult {
             proof,
             next_cmt,
@@ -288,12 +351,45 @@ mod tests {
         )
         .unwrap();
 
-        println!("Next commitment: {}", next_cmt);
-        println!("Next commitment randomness: {}", next_cmt_r);
-        println!("Proof (hex): {}", hex::encode(&proof));
-        println!("Proof length: {}", proof.len());
-        assert!(!proof.is_empty());
+        assert!(!proof_with_public_inputs.proof.is_empty());
         assert_eq!(next_cmt.len(), 64); // 32 bytes in hex
         assert_eq!(next_cmt_r.len(), 64); // 32 bytes in hex
+        assert_eq!(
+            proof_with_public_inputs.num_public_inputs,
+            9 // Number of public inputs expected for es256_ca
+        );
+
+        // Generate next commitment and check it matches
+        let subject = vec![
+            0x30, 0x1f, 0x31, 0x1d, 0x30, 0x1b, 0x06, 0x03, 0x55, 0x04, 0x03, 0x13, 0x14, 0x41,
+            0x6e, 0x64, 0x72, 0x6f, 0x69, 0x64, 0x20, 0x4b, 0x65, 0x79, 0x73, 0x74, 0x6f, 0x72,
+            0x65, 0x20, 0x4b, 0x65, 0x79,
+        ]; // CN=Android Keystore Key
+        let subject_key_id = vec![
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        ]; // Empty key identifier
+        let subject_pk_x = vec![
+            0xb4, 0x46, 0x2b, 0xe1, 0x47, 0x16, 0x55, 0x9d, 0x26, 0xf1, 0x2e, 0x60, 0x4f, 0xed,
+            0xe1, 0x53, 0x39, 0xd2, 0x5a, 0xa4, 0xf5, 0xdb, 0xda, 0x49, 0x6e, 0x1f, 0x30, 0x43,
+            0x36, 0x01, 0xed, 0x74,
+        ];
+        let subject_pk_y = vec![
+            0xf6, 0x39, 0x6f, 0x87, 0xe8, 0xe7, 0x20, 0x55, 0x3d, 0x86, 0x22, 0xa1, 0xbb, 0xd7,
+            0xab, 0xf5, 0x01, 0x19, 0x1b, 0xae, 0x74, 0x94, 0x97, 0x86, 0x76, 0x47, 0x6b, 0x00,
+            0xfb, 0xd6, 0xda, 0x90,
+        ];
+        let CommitResult {
+            cmt: next_cmt_generated,
+            r: _,
+        } = commit_attrs(
+            &subject,
+            &subject_key_id,
+            &subject_pk_x,
+            &subject_pk_y,
+            Some(&next_cmt_r),
+        )
+        .unwrap();
+        assert_eq!(next_cmt, next_cmt_generated);
     }
 }
